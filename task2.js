4f7258a84811eeff87971ec67c15b0b0b86918f8
ITLeopard <foret_spbu@mail.ru>
2018-06-05 23:33:41 +0300

    Solved task 2

---
+++ task2.js
@@ -0,0 +1,25 @@
+var str_a = process.argv[2]; 
+var str_b = process.argv[3];
+
+var str = str_a;
+var count = -1;
+
+for(i=0; i<str.length; i++)
+{
+	str = str.replace(str[0],"")+str[0];
+	if(str == str_b)
+	{
+		count = i+1;
+		break;
+	}
+}
+for(i=0; i<str.length; i++)
+{
+	str = str[str.length-1]+str.replace(str[str.length-1],"");
+	if(str == str_b)
+	{
+		if (i+1 < count) count = i+1;
+		break;
+	}
+}
+process.stdout.write(String(count));
